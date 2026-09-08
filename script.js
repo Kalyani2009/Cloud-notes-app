6a433c77d2dca06e1b29c5e7134df4c164789e75
Kalyani2009 <balubanoth4193@gmail.com>
2026-09-08 13:30:56 +0530

    Refactor Firebase initialization and configuration
    Updated Firebase SDK imports to use specific version URLs and added databaseURL to the configuration.

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBcYtsTZmv_R2Zo0QqsTDjt9iRgF38qeoU",
-  authDomain: "python-7ab93.firebaseapp.com",
-  projectId: "python-7ab93",
-  storageBucket: "python-7ab93.firebasestorage.app",
-  messagingSenderId: "258064448774",
-  appId: "1:258064448774:web:032eafabbfd69e6206f287",
-  measurementId: "G-K6HD0K00CK"
-};
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBcYtsTZmv_R2Zo0QqsTDjt9iRgF38qeoU",
+    authDomain: "python-7ab93.firebaseapp.com",
+    databaseURL: "https://python-7ab93-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "python-7ab93",
+    storageBucket: "python-7ab93.firebasestorage.app",
+    messagingSenderId: "258064448774",
+    appId: "1:258064448774:web:032eafabbfd69e6206f287",
+    measurementId: "G-K6HD0K00CK"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
